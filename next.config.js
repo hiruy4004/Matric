38e9e0e38a52b5e83e25250c2ecca88ac11b9e5a
hiruy4004 <hiruy4004@gmail.com>
2025-02-19 09:48:15 -0800

    though

--- next.config.js
+++ next.config.js
@@ -1,15 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: true,
   swcMinify: true,
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '**',
-      },
+        hostname: '**'
+      }
     ],
   }
 }
 
-module.exports = nextConfig 
+module.exports = nextConfig
